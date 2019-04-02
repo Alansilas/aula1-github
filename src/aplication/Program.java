01c08966751544a6fd54b5a73d59dd7a4bcd66ff
Alan Marques <alan.smak@gmail.com>
2019-04-02 17:32:12 -0300

    msg de boa noite e identacao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -9,27 +9,28 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		int [][] matriz = new int[n][n];
-		
-		for(int i = 0;i <  matriz.length; i++) {
-			for(int j = 0; j < matriz.length; j ++) {
+		int[][] matriz = new int[n][n];
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
 				matriz[i][j] = sc.nextInt();
 			}
 		}
 		System.out.println("-------------------");
-		for(int i = 0;i <  matriz.length; i++) {
-			for(int j = 0; j < matriz.length; j ++) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
 				System.out.println(matriz[i][j]);
 			}
 		}
 		System.out.println("---------------------");
-		for(int i = 0; i < matriz.length; i++) {
+		for (int i = 0; i < matriz.length; i++) {
 			System.out.println(matriz[i][i]);
 		}
 		sc.close();
 		System.out.println("Boa tarde!!!");
+		System.out.println("Boa noite!!!");
 	}
 
 }
